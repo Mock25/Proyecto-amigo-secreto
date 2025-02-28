2d74d9362f83ddf6459f150b7f4a19d7e4064c71
Mock 25 <apexfer@hotmail.com>
2025-02-28 08:46:06 -0600

    no hice nada pero lo intente

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let nombreDeAmigos = [];
+
+
+document.getElementById(amigo);
+function agregarAmigo() {
+    let amigo = document.getElementById(amigo).value;
+    nombreDeAmigos.push(amigo);
+    console.log(nombreDeAmigos);
+}
+
+function mostrarAmigos() {
+    let amigos = document.getElementById('amigos');
+    amigos.innerHTML = nombreDeAmigos;
+}
+
+function buscarAmigo() {
+    let amigo = document.getElementById('amigo').value;
+    let amigos = document.getElementById('amigos');
+    amigos.innerHTML = nombreDeAmigos;
+    if (nombreDeAmigos.includes(amigo)) {
+        amigos.innerHTML = 'Tu amigo está en la lista';
+    } else {
+        amigos.innerHTML = 'Tu amigo no está en la lista';
+    }
+}
+
